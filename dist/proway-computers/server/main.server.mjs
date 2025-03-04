edea57f56632f33f098c3e5c681b80bfaf822dbd
José Luiz <zeluizjsantos@gmail.com>
2025-03-03 21:48:21 -0300

    build2

---
+++ dist/proway-computers/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UQXTMVZJ.mjs";import"./chunk-INW5X736.mjs";import"./chunk-5RQF3VXE.mjs";import"./chunk-YX7DITRD.mjs";import"./chunk-J4BDXTMW.mjs";import"./chunk-UNE4RRSI.mjs";import"./chunk-OL2TAVVP.mjs";import"./chunk-AQWE33PF.mjs";import"./chunk-7T7I53IS.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
